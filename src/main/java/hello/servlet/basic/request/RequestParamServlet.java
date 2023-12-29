6577d996011ba6ff39164d3dd3d9e23494a19b47
han16935 <han1693516@gmail.com>
2023-12-29 20:02:09 +0900

    Servlet 통한 url parameter 가져오기 연습

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+
+        String[] names = request.getParameterValues("username");
+        for (String name : names) {
+            System.out.println(name);
+        }
+        response.getWriter().write("ok!");
+    }
+}
