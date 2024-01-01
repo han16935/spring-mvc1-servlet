b3c15d28d9a27de0d270eb4fac78e8f0e7efbcb1
han16935 <han1693516@gmail.com>
2024-01-01 16:43:58 +0900

    OldController에 ModelAndView 추가

--- src/main/java/hello/servlet/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/springmvc/old/OldController.java
@@ -5,12 +5,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("bean name handler");
-        return null;
+        return new ModelAndView("new-form");
     }
 }
